1eb2a31229c3c9d52a3b52b1babf1eb1bddfe2c1
Sachin-Mahesh-J <sachinjayathilaka884@gmail.com>
2025-03-01 20:43:56 +0530

    login screen and admin dashbord ui upgrade

---
+++ Retail_Managment_WPF_UI/Views/AdminDashbordView.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Retail_Managment_WPF_UI.Views
+{
+    /// <summary>
+    /// Interaction logic for AdminDashbordView.xaml
+    /// </summary>
+    public partial class AdminDashbordView : Window
+    {
+        public AdminDashbordView()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Dashboard Clicked", "Dashboard", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Users_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Users Clicked", "Users", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Settings Clicked", "Settings", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Logging Out...", "Logout", MessageBoxButton.OK, MessageBoxImage.Information);
+            LoginView loginView = new LoginView();
+            loginView.Show();
+            this.Close();
+            
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // Close the window
+        }
+
+        // Enable window dragging
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                MaximizeButton.Content = "□"; // Restore symbol
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+                MaximizeButton.Content = "❐"; // Indicate maximized state
+            }
+        }
+    }
+}
---
+++ Retail_Managment_WPF_UI/Views/LoginView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Retail_Managment_WPF_UI.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            if (username == "admin" && password == "1234") // Replace with DB authentication
+            {
+                MessageBox.Show("Login Successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                AdminDashbordView adminDashbordView = new AdminDashbordView();
+                adminDashbordView.Show();
+                this.Close();
+                
+            }
+            else
+            {
+                MessageBox.Show("Invalid Credentials!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+    }
+}
